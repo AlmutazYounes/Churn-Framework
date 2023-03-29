34c31c842cfbd3737f84fe485dfbdf07478fd7bc
MutazYounes <mutazyounes@gmail.com>
2023-03-29 03:28:54 -0500

    V2 - small changes

--- ChurnPredictor.py
+++ ChurnPredictor.py
@@ -1,36 +1,39 @@
-from Utils.config import Config, run_params
-from Utils.util import Util
+import logging
+
 from features.feature_loader import FeatureLoader
 from automl.H2oAutoMl import AutoML
-# from automl.Automl import AutoML
-
+from Utils.config import Config, run_params
+from Utils.util import Util
 
 class ChurnPredictor:
     def __init__(self):
+        self.logger = logging.getLogger(__name__)
         self.feature_definitions = Util.get_feature_definitions(Config.features_type)
+        self.train_data = None
+        self.test_data = None
 
     def extract_features(self, sampling_method, ratio):
         self.train_data, self.test_data = Util.load_data(Config.features_path)
         feature_extractor = FeatureLoader(self.feature_definitions, sampling_method, ratio)
-        self.train_data, self.test_data = feature_extractor.preprocessing(self.train_data, self.test_data)
-
+        self.train_data, self.test_data = feature_extractor.preprocess_data(self.train_data, self.test_data)
         self.train_data, self.test_data = feature_extractor.extract_features(self.train_data, self.test_data)
-        feature_extractor.save_features(self.train_data, self.test_data, sampling_method, ratio)
-        return self.train_data, self.test_data
+        feature_extractor.save_features(self.train_data, self.test_data)
+        self.logger.info(f"Features extracted using {sampling_method} and ratio {ratio}")
 
-    def train_model(self, train_data, test_data, sampling_method, ratio):
-        AutoML(train_data, test_data, f"Output/{sampling_method}_{ratio}").fit()
+    def train_model(self, sampling_method, ratio):
+        model = AutoML(self.train_data, self.test_data, f"Output/{sampling_method}_{ratio}")
+        model.fit()
+        self.logger.info(f"Model trained using {sampling_method} and ratio {ratio}")
 
     def run(self):
         for sampling_method, ratio_list in run_params.sampling.items():
             for ratio in ratio_list:
-                print(
-                    f" ##################################### {sampling_method} : {ratio} ##################################### ")
-                train_data, test_data = self.extract_features(sampling_method, ratio)
-                self.train_model(train_data, test_data, sampling_method, ratio)
-
+                self.logger.info(
+                    f"##################################### {sampling_method} : {ratio} #####################################")
+                self.extract_features(sampling_method, ratio)
+                self.train_model(sampling_method, ratio)
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     churn_predictor = ChurnPredictor()
     churn_predictor.run()
-
--- features/feature_loader.py
+++ features/feature_loader.py
@@ -5,45 +5,59 @@ from Utils.config import Config, run_params
 from Utils.util import Util
 from features.feature_engineer import FeatureEngineer
 
-
 class FeatureLoader:
-    def __init__(self, feature_definitions, sampling_method, sample_ratio):
-        self.feature_definitions = feature_definitions
+    def __init__(self, feature_defs, sampling_method, sample_ratio):
+        """
+        Initializes a FeatureLoader object with feature definitions, sampling method, and sample ratio.
+        """
+        self.feature_defs = feature_defs
         self.sampling_method = sampling_method
         self.sample_ratio = sample_ratio
 
-    def feature_extraction_methods(self, data, feature, type_, **conf):
-        if type_ == "absolute":
+    def extract_feature(self, data, feature, feature_type, **conf):
+        """
+        Extracts a specific feature from the given data using the appropriate feature extraction method.
+        """
+        if feature_type == "absolute":
             return Util.absolute(data, feature, **conf)
-        if type_ == "categorical_encoding":
+        if feature_type == "categorical_encoding":
             return Util.categorical_encoding(data, feature, **conf)
-        if type_ == "label":
+        if feature_type == "label":
             return Util.label(data, feature, **conf)
 
     def extract_features(self, train_data, test_data):
+        """
+        Extracts all the defined features from the given train and test data.
+        """
         train_features = pd.DataFrame()
         test_features = pd.DataFrame()
-        # Train Data
-        for feature in self.feature_definitions:
-            feature_params = self.feature_definitions[feature]
-            conf = self.feature_definitions[feature]["conf"]
-            train_feature = self.feature_extraction_methods(train_data, feature, feature_params["type"], **conf)
+        for feature in self.feature_defs:
+            feature_params = self.feature_defs[feature]
+            conf = feature_params["conf"]
+            train_feature = self.extract_feature(train_data, feature, feature_params["type"], **conf)
             train_features = pd.concat([train_features, pd.DataFrame(train_feature)], axis=1)
 
-            # Test Data
-            test_feature = self.feature_extraction_methods(test_data, feature, feature_params["type"], **conf)
+            test_feature = self.extract_feature(test_data, feature, feature_params["type"], **conf)
             test_features = pd.concat([test_features, pd.DataFrame(test_feature)], axis=1)
 
         fe = FeatureEngineer(self.sampling_method, self.sample_ratio, run_params.missing_values_numarical)
-        train_features = fe.preprocess(train_features, self.feature_definitions, data_type="train", sampleing=True)
-        test_features = fe.preprocess(test_features, self.feature_definitions, data_type="test", sampleing=False)
+        train_features = fe.preprocess(train_features, self.feature_defs, data_type="train", sampleing=True)
+        test_features = fe.preprocess(test_features, self.feature_defs, data_type="test", sampleing=False)
         return train_features, test_features
 
-    def preprocessing(self, train_data, test_data):
+    def preprocess_data(self, train_data, test_data):
+        """
+        Preprocesses the given train and test data by replacing any space characters with NaN values.
+        """
         train_data = train_data.replace(" ", np.nan)
         test_data = test_data.replace(" ", np.nan)
         return train_data, test_data
 
-    def save_features(self, train_data, test_data, sampling_method, ratio):
-        train_data.to_csv(f"{Config.features_train_output_file}_{sampling_method}_{ratio}.csv", index=False)
-        test_data.to_csv(f"{Config.features_test_output_file}_{sampling_method}_{ratio}.csv", index=False)
+    def save_features(self, train_data, test_data):
+        """
+        Saves the extracted train and test features to CSV files using the defined naming convention.
+        """
+        train_file_name = f"{Config.features_train_output_file}_{self.sampling_method}_{self.sample_ratio}.csv"
+        test_file_name = f"{Config.features_test_output_file}_{self.sampling_method}_{self.sample_ratio}.csv"
+        train_data.to_csv(train_file_name, index=False)
+        test_data.to_csv(test_file_name, index=False)
